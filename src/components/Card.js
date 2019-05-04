98d18949189d4536fc9c0259b7d26b619a33e0fe
ralonsodeniz <ralonsodeniz@gmail.com>
2019-05-04 20:32:34 +0200

    updated card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Card extends Component {
-  updateElement = () => {
-    // to be able to use the callback function with a parameter from the parent we need to use an auxiliary function because if not it would be run once it reads the dom code
-    this.props.onActiveDetail(this.props.element);
-  };
-
-  render() {
-    return (
-      <div
-        className="card tc bg-light-green dib br3 ma2 grow bw2 shadow-5 pa2"
-        onClick={this.updateElement}
-      >
-        {(() => {
-          switch (this.props.source) {
-            case "films":
-              return <h2>{this.props.element.title.toLowerCase()}</h2>;
-            case "people":
-              return <h2>{this.props.element.name.toLowerCase()}</h2>;
-            case "planets":
-              return <h2>{this.props.element.name.toLowerCase()}</h2>;
-            case "species":
-              return <h2>{this.props.element.name.toLowerCase()}</h2>;
-            case "starships":
-              return <h2>{this.props.element.name.toLowerCase()}</h2>;
-            case "vehicles":
-              return <h2>{this.props.element.name.toLowerCase()}</h2>;
-            default:
-              return null;
-          }
-        })()}
-      </div>
-    );
-  }
-}
+const Card = props => {
+  // to be able to use the callback function with a parameter from the parent we need to use an auxiliary function because if not it would be run once it reads the dom code
+  return (
+    <div
+      className="card tc bg-light-green dib br3 ma2 grow bw2 shadow-5 pa2"
+      onClick={() => props.onActiveDetail(props.element)}
+    >
+      {(() => {
+        switch (props.source) {
+          case "films":
+            return <h2>{props.element.title.toLowerCase()}</h2>;
+          case "people":
+            return <h2>{props.element.name.toLowerCase()}</h2>;
+          case "planets":
+            return <h2>{props.element.name.toLowerCase()}</h2>;
+          case "species":
+            return <h2>{props.element.name.toLowerCase()}</h2>;
+          case "starships":
+            return <h2>{props.element.name.toLowerCase()}</h2>;
+          case "vehicles":
+            return <h2>{props.element.name.toLowerCase()}</h2>;
+          default:
+            return null;
+        }
+      })()}
+    </div>
+  );
+};
 export default Card;
